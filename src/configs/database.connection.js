822deba86574ab0bb51b2dbcae617505d173b939
thiago-arend <thiago.arend@gmail.com>
2023-09-11 17:49:45 -0300

    fix: config para o deploy

--- src/configs/database.connection.js
+++ src/configs/database.connection.js
@@ -4,6 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pg;
-export const db = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
